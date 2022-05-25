12c7a97d6141c2caf4354c19931b69e08951ea76
AhmedANafees <91854796+AhmedANafees@users.noreply.github.com>
2022-05-25 14:19:23 -0400

    added wrong file

--- CoinFlipper.java
+++ CoinFlipper.java
@@ -1,74 +1,80 @@
+package CoinFlipperGUI;
+
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.Set;
 
 
 /**
  *
  * @author nafea8846
  */
-public class ClickCounter  implements Runnable, ActionListener{
+public class CoinFlipper  implements Runnable, ActionListener{
 
   // Class Variables  
   JPanel mainPanel;
-  JLabel Label;
-  JButton clicker;
-  JTextField counter;
-  int i = 0;
+  JLabel label;
+  JButton flipper;
+  JTextField result;
 
-  
 
   // Method to assemble our GUI
   public void run(){
     // Creats a JFrame that is 800 pixels by 600 pixels, and closes when you click on the X
-    JFrame frame = new JFrame("Click Counter");
+    JFrame frame = new JFrame("Coin Flipper");
     // Makes the X button close the program
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     // makes the windows 800 pixel wide by 600 pixels tall
-    frame.setSize(300,200);
+    frame.setSize(250,200);
     // shows the window
     frame.setVisible(true);
-    
+ 
     mainPanel = new JPanel();
     mainPanel.setLayout(null);
- 
-    Label = new JLabel("Number of Button Clicks:");
-    Label.setBounds(20, 100, 150, 50);
     
-    counter = new JTextField();
-    counter.setBounds(175, 100, 50, 50);
+    label = new JLabel("The coin is: ");
+    label.setBounds(25, 25, 100, 50);
     
-    clicker = new JButton("Click here");
-    clicker.setBounds(90, 25, 100, 50);
-    clicker.addActionListener(this);
-    clicker.setActionCommand("clicks");
+    result = new JTextField();
+    result.setBounds(100, 40, 100, 25);
     
-    mainPanel.add(clicker);
-    mainPanel.add(counter);
-    mainPanel.add(Label);
-    frame.add(mainPanel);
+    flipper = new JButton("Flip Coin");
+    flipper.setBounds(60, 75, 100, 50);
+    flipper.addActionListener(this);
+    flipper.setActionCommand("Flip");
+
+    mainPanel.add(flipper);
+    mainPanel.add(result);
+    mainPanel.add(label);
+    frame.add(mainPanel);    
   }
 
   // method called when a button is pressed
   public void actionPerformed(ActionEvent e){
     // get the command from the action
-    
     String command = e.getActionCommand();
-    
-    if(command.equals("clicks"))
+    if (command.equals("Flip"))
     {
-      i++;
-      counter.setText(" " + i);
+        int lowest = 1;
+        int highest = 2;
+        int randomNumber = (int)(Math.random( )*(highest-lowest+1) + lowest);
+    
+        if (randomNumber == 1)
+        {
+            result.setText("Heads");
+        }
+        else
+        {
+            result.setText("Tails");
+        }
     }
-
   }
 
   // Main method to start our program
   public static void main(String[] args){
     // Creates an instance of our program
-    ClickCounter  gui = new ClickCounter ();
+    CoinFlipper  gui = new CoinFlipper ();
     // Lets the computer know to start it in the event thread
     SwingUtilities.invokeLater(gui);
   }
